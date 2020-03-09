2100b12273091b03598a9b253bec907865baeb63
Carl Alden Go <alden.go@obf.ateneo.edu>
2020-03-10 01:35:11 +0800

    Initial Commit

---
+++ heroes/tests.py
@@ -0,0 +1,12 @@
+from django.urls import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+
+from heroes.views import home_page
+
+
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home_page.html')
